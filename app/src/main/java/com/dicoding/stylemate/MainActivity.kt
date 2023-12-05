51f1762b5a61f5e19b49e1f869e942fc55802cf5
Rohmat Ubaidillah Fahmi <rohmatubaiddillahf28@gmail.com>
2023-12-05 11:26:59 +0700

    menambah activity fitur scan, profile dan home

--- app/src/main/java/com/dicoding/stylemate/MainActivity.kt
+++ app/src/main/java/com/dicoding/stylemate/MainActivity.kt
@@ -2,10 +2,37 @@ package com.dicoding.stylemate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.dicoding.stylemate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(Home())
+                R.id.scan -> replaceFragment(Scan())
+                R.id.profil -> replaceFragment(Profile())
+                else -> {
+
+                }
+            }
+            true
+        }
+
+
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
---
+++ app/src/main/java/com/dicoding/stylemate/scan/ScanActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.stylemate.scan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.stylemate.R
+
+class ScanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan)
+    }
+}
